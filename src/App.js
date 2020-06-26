5a933173976aa9b73bdaf4a912eed24aedfe5350
Amar Gupta <amar9911056196@gmail.com>
2020-06-26 16:42:27 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import Navigation from './components/navigation';
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
-import PageRender from './page-render'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import PageRender from './page-render';
+
 
 function App() {
   const user = {
-    firstname: "Amar",
-    lastname: "Coder"
-  }
+    firstname: 'Amar',
+    lastname: 'Coder',
+  };
   return (
     <Router>
-    <div className="App">
-      <Navigation user={user} />
-      <Switch>
-        <Route path="/:page" component={PageRender} />
-        <Route path="/" render={()=> <Redirect to="/home" />} />
-        <Route component={()=> 404} />
-      </Switch>
-    </div>
+      <div className="App">
+        <Navigation user={user} />
+        <Switch>
+          <Route path="/:page" component={PageRender} />
+          <Route path="/" render={() => <Redirect to="/home" />} />
+          <Route component={() => 404} />
+        </Switch>
+      </div>
     </Router>
   );
 }
